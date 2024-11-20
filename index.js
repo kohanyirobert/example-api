c29c9bfacb69e29fc9569183658af966e2c6a9b2
Kohányi Róbert <kohanyi.robert@gmail.com>
2024-11-20 11:01:57 +0100

    Reformat, add HOST

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import pgPromise from 'pg-promise'
 import pgConnectionString from 'pg-connection-string'
 
+console.log('HOST:', process.env.HOST)
 console.log('PORT:', process.env.PORT)
 
 const pgp = pgPromise()
@@ -14,54 +15,54 @@ console.log('Hostname:', os.hostname())
 
 const networkInterfaces = os.networkInterfaces()
 for (const interfaceName in networkInterfaces) {
-    const interfaces = networkInterfaces[interfaceName]
-    for (const iface of interfaces) {
-        if (iface.family === 'IPv4' && !iface.internal) {
-            console.log(`IPv4: ${iface.address}`)
-        }
+  const interfaces = networkInterfaces[interfaceName]
+  for (const iface of interfaces) {
+    if (iface.family === 'IPv4' && !iface.internal) {
+      console.log(`IPv4: ${iface.address}`)
     }
+  }
 }
 
 const app = express()
 app.use(express.json())
 
 app.get('/api/cars', async (req, res) => {
-    try {
-        console.log('GET /api/cars')
-        const cars = await db.any('SELECT * FROM car')
-        console.log('Result:', cars)
-        res.json(cars)
-    } catch (e) {
-        console.error(e)
-        res.status(500).send(e)
-    }
+  try {
+    console.log('GET /api/cars')
+    const cars = await db.any('SELECT * FROM car')
+    console.log('Result:', cars)
+    res.json(cars)
+  } catch (e) {
+    console.error(e)
+    res.status(500).send(e)
+  }
 })
 
 app.post("/api/cars", async (req, res) => {
-    try {
-        console.log('POST /api/cars', req.body)
-        const { id } = await db.one('INSERT INTO car (manufacturer, model) VALUES ($1, $2) RETURNING id', [
-            req.body.manufacturer,
-            req.body.model
-        ])
-        const car = { id, ...req.body }
-        console.log('Result:', car)
-        res.json(car)
-    } catch (e) {
-        console.error(e)
-        res.status(500).send(e)
-    }
+  try {
+    console.log('POST /api/cars', req.body)
+    const { id } = await db.one('INSERT INTO car (manufacturer, model) VALUES ($1, $2) RETURNING id', [
+      req.body.manufacturer,
+      req.body.model
+    ])
+    const car = { id, ...req.body }
+    console.log('Result:', car)
+    res.json(car)
+  } catch (e) {
+    console.error(e)
+    res.status(500).send(e)
+  }
 })
 
 app.delete("/api/cars/:id", async (req, res) => {
-    try {
-        console.log(`DELETE /api/cars/${req.params.id}`)
-        await db.none('DELETE FROM car WHERE id = $1', [+req.params.id])
-        res.send()
-    } catch (e) {
-        console.error(e)
-        res.status(500).send(e)
-    }
+  try {
+    console.log(`DELETE /api/cars/${req.params.id}`)
+    await db.none('DELETE FROM car WHERE id = $1', [+req.params.id])
+    res.send()
+  } catch (e) {
+    console.error(e)
+    res.status(500).send(e)
+  }
 })
 
-app.listen(process.env.PORT, () => console.log('Listening on port', process.env.PORT))
+app.listen(process.env.PORT, process.env.HOST, () => console.log(`Listening on ${process.env.HOST}:${process.env.PORT}`))
